b5cdda5c6bab0142714d43257b5097fc73ad7794
alexandervas <vasyutin4@gmail.com>
2017-03-15 12:58:43 +0200

    attachment test

---
+++ tests/AttachmentTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace alexandervas\slackbotlistener\tests;
+
+use alexandervas\slackbotlistener\Attachment;
+use PHPUnit\Framework\TestCase;
+
+class AttachmentTest extends TestCase{
+
+    /**
+     * @var string $attach
+     */
+    public $attach;
+
+    /**
+     * @var array $attachment_options
+     */
+    public $attachment_options = array();
+
+    public function setUp()
+    {
+        $this->attachment_options = array(
+            'fallback' => 'fallback test',
+            'author_name' => 'bobby',
+            'author_link' => 'http://flickr.com/bobby/',
+            'author_icon' => 'http://flickr.com/icons/bobby.jpg',
+            'title' => 'Optional title',
+            'text'  => 'optional text',
+            'pretext' => 'optional pretext',
+            'image_url' => 'http://my-website.com/path/to/image.jpg',
+            'thumb_url' => 'http://example.com/path/to/thumb.png'
+        );
+    }
+
+    public function testCanSerializeAttachmentOptionsFallback(){
+
+        $attachment = new Attachment('test_fallback');
+        $this->assertEquals(array('fallback'=>'test_fallback'),$attachment->serialize());
+    }
+
+    public function testCanSerializeAttachmentOptions(){
+        $attachment = new Attachment('fallback test');
+
+        foreach ($this->attachment_options as $key => $value){
+            $attachment->applyOptions($key,$value);
+        }
+        $this->assertEquals($this->attachment_options, $attachment->serialize());
+    }
+}
